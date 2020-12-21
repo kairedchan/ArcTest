9fe34bfc2f57dbff54d5a4b2c6db2ef2fd08792a
kairedchan@qq.com <Chengkai11011>
2020-12-21 17:01:54 +0800

    第一个GitHub项目

---
+++ StudySpringBoot/src/main/java/com/greatmap/arc/Demo1221.java
@@ -0,0 +1,16 @@
+package com.greatmap.arc;
+
+/**
+ * Company: 武汉天耀宏图科技有限公司
+ * Project name: ArcTest
+ * Create Time: 2020-12-21
+ * Author 陈凯红 chenkaihong@greatemap.com.cn
+ * Description
+ **/
+public class Demo1221 {
+
+    public static void main(String[] args) {
+        System.out.println("第一个GitHub项目");
+    }
+
+}
